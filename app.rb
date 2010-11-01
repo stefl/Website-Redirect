29929d2cd322c96473f35c4b0ab2b4b5b3d6a9a4
steflewandowski <stef@stef.io>
2010-11-01 19:42:03 +0000

    duh - file path load

--- app.rb
+++ app.rb
@@ -1,9 +1,8 @@
 require 'sinatra'
-
 configure do
-  @config = YAML::load(File.join(File.dirname(__FILE__), "config.yaml"))
+  @@config = YAML::load(File.open(File.join(File.dirname(__FILE__), "config.yaml")).read)
 end
-
 get %r{/*/} do
-  redirect "http://#{@config[:to]}#{request.path}"
+  puts @@config.inspect
+  redirect "http://#{@@config[:to]}#{request.path}"
 end
