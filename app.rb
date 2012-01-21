212478d342ee1417c6b6b000d8e0711384a14172
steflewandowski <stef@stef.io>
2012-01-21 16:54:29 +0000

    remove app.rb

--- app.rb
+++
@@ -1,7 +0,0 @@
-configure do
-  @@config = YAML::load(File.open(File.join(File.dirname(__FILE__), "config.yaml")).read)
-end
-get %r{/*/} do
-  puts @@config.inspect
-  redirect "http://#{@@config[:to]}#{request.path}"
-end
